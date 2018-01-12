6f858cec97fa9ba17826f806c5b42ab425f35e0f
Andrew J Westlake <kelixes@gmail.com>
2018-01-12 01:55:49 -0600

    hopefully fixed all of the issues with travis builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ cargo publish --token $CARGO_TOKEN || exit 1
 
 VERSION=`cargo pkgid | sed -E 's/.*#(.*:)?(.+)/\2/'`
 git tag v$VERSION || exit 2
-git push https://awestlake87:$GITHUB_TOKEN@github.com/awestlake87/sc2-proto-rs v$VERSION || exit 3
+git push https://awestlake87:$GITHUB_TOKEN@github.com/awestlake87/sc2-proto-rs.git v$VERSION || exit 3
