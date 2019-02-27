9fd0be3197c4d411906b64099a1eed787691799b
Hannes Karppila <hannes.karppila@gmail.com>
2019-02-27 13:03:57 +0200

    Compile protobufs to Cargo OUT_DIR instead of src/

--- build.rs
+++ build.rs
@@ -1,21 +1,46 @@
 extern crate protoc_rust;
 
+use std::env;
+use std::ffi::OsStr;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn proto_modules(proto_dir: &Path) -> Vec<String> {
+    fs::read_dir(proto_dir)
+        .expect("Could not read protobuf directory")
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+            if path.is_file() && path.extension() == Some(OsStr::new("proto")) {
+                path.file_stem()
+                    .and_then(|n| n.to_os_string().into_string().ok())
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
 fn main() {
+    let in_dir = "./s2client-proto/s2clientprotocol";
+    let out_dir = &env::var("OUT_DIR").unwrap();
+
+    // Read list of all input protobuf files
+    let input_mods = proto_modules(Path::new(in_dir));
+    let input_files: Vec<String> = input_mods
+        .iter()
+        .map(|s| format!("{}/{}.proto", in_dir, s))
+        .collect();
+
+    // Compile protocol buffers
     if let Err(e) = protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/",
+        out_dir,
         includes: &["s2client-proto/"],
-        input: &[
-            "s2client-proto/s2clientprotocol/common.proto",
-            "s2client-proto/s2clientprotocol/data.proto",
-            "s2client-proto/s2clientprotocol/debug.proto",
-            "s2client-proto/s2clientprotocol/error.proto",
-            "s2client-proto/s2clientprotocol/query.proto",
-            "s2client-proto/s2clientprotocol/raw.proto",
-            "s2client-proto/s2clientprotocol/sc2api.proto",
-            "s2client-proto/s2clientprotocol/score.proto",
-            "s2client-proto/s2clientprotocol/spatial.proto",
-            "s2client-proto/s2clientprotocol/ui.proto",
-        ],
+        input: input_files
+            .iter()
+            .map(|s| s.as_str())
+            .collect::<Vec<_>>()
+            .as_slice(),
         customize: protoc_rust::Customize {
             ..Default::default()
         },
@@ -24,4 +49,17 @@ fn main() {
     } else {
         println!("protobufs were generated successfully");
     }
+
+    // Generate the lib.rs source code
+    let mut buffer = fs::File::create(format!("{}/{}", out_dir, "lib.rs")).unwrap();
+    buffer
+        .write(
+            input_mods
+                .iter()
+                .map(|s| format!("pub mod {};", s))
+                .collect::<Vec<_>>()
+                .join("\n")
+                .as_bytes(),
+        )
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
-extern crate protobuf;
+//! Protocol buffers for the StarCraft II Client API
+//! Automatically generated from the protobuf definitions at
+//! [https://github.com/Blizzard/s2client-proto](https://github.com/Blizzard/s2client-proto).
 
-pub mod common;
-pub mod data;
-pub mod debug;
-pub mod error;
-pub mod query;
-pub mod raw;
-pub mod sc2api;
-pub mod score;
-pub mod spatial;
-pub mod ui;
+// All magic is done in the build.rs, this just includes the results
+include!(concat!(env!("OUT_DIR"), "/lib.rs"));
