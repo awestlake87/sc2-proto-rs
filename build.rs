b57c8fa82c1f57008811905caec35f9fc45947fc
Armageddon <lunyx1337@gmail.com>
2021-02-03 21:00:11 +0300

    build skipping codegen by default, add 'expose_fields' codegen option, regenerate *.rs files

--- build.rs
+++ build.rs
@@ -1,11 +1,9 @@
-extern crate protoc_rust;
+#[cfg(feature = "protoc-rust")]
+use protoc_rust::{Codegen, Customize};
+#[cfg(feature = "protoc-rust")]
+use std::{env, ffi::OsStr, fs, path::Path};
 
-use std::env;
-use std::ffi::OsStr;
-use std::fs;
-use std::io::prelude::*;
-use std::path::Path;
-
+#[cfg(feature = "protoc-rust")]
 fn proto_modules(proto_dir: &Path) -> Vec<String> {
     fs::read_dir(proto_dir)
         .expect("Could not read protobuf directory")
@@ -21,6 +19,7 @@ fn proto_modules(proto_dir: &Path) -> Vec<String> {
         .collect()
 }
 
+#[cfg(feature = "protoc-rust")]
 fn main() {
     let in_dir = "./s2client-proto/s2clientprotocol";
     let out_dir = &env::var("OUT_DIR").unwrap();
@@ -33,33 +32,33 @@ fn main() {
         .collect();
 
     // Compile protocol buffers
-    if let Err(e) = protoc_rust::run(protoc_rust::Args {
-        out_dir,
-        includes: &["s2client-proto/"],
-        input: input_files
-            .iter()
-            .map(|s| s.as_str())
-            .collect::<Vec<_>>()
-            .as_slice(),
-        customize: protoc_rust::Customize {
+    if let Err(e) = Codegen::new()
+        .out_dir(out_dir)
+        .include("s2client-proto/")
+        .inputs(input_files)
+        .customize(Customize {
+            expose_fields: Some(true),
             ..Default::default()
-        },
-    }) {
+        })
+        .run()
+    {
         panic!("{:#?}", e);
-    } else {
-        println!("protobufs were generated successfully");
     }
+    println!("protobufs were generated successfully");
 
     // Generate the lib.rs source code
-    let mut buffer = fs::File::create(format!("{}/{}", out_dir, "lib.rs")).unwrap();
-    buffer
-        .write(
-            input_mods
-                .iter()
-                .map(|s| format!("pub mod {};", s))
-                .collect::<Vec<_>>()
-                .join("\n")
-                .as_bytes(),
-        )
-        .unwrap();
+    fs::write(
+        format!("{}/{}", out_dir, "lib.rs"),
+        input_mods
+            .iter()
+            .map(|s| format!("pub mod {};", s))
+            .collect::<Vec<_>>()
+            .join("\n"),
+    )
+    .unwrap();
+}
+
+#[cfg(not(feature = "protoc-rust"))]
+fn main() {
+    println!("using pre-generated *.rs files in 'src/'");
 }
