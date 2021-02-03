5a445e1544d817fe22277783229d3e0545f207df
Armageddon <lunyx1337@gmail.com>
2021-02-03 21:00:11 +0300

    build skipping codegen by default, add 'expose_fields' codegen option, regenerate *.rs files

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,26 @@
 //! [https://github.com/Blizzard/s2client-proto](https://github.com/Blizzard/s2client-proto).
 
 // All magic is done in the build.rs, this just includes the results
+#[cfg(feature = "protoc-rust")]
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+
+macro_rules! cfg_no_protoc {
+    ($($item:item)+) => {
+        $(
+            #[cfg(not(feature = "protoc-rust"))]
+            $item
+        )*
+    };
+}
+cfg_no_protoc!(
+    pub mod common;
+    pub mod data;
+    pub mod debug;
+    pub mod error;
+    pub mod query;
+    pub mod raw;
+    pub mod sc2api;
+    pub mod score;
+    pub mod spatial;
+    pub mod ui;
+);
